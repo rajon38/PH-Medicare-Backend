26ac2242ba94430121b03a80e98328ac479779e9
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-02-20 22:10:53 +0600

    Implement patient profile update functionality; add validation, middleware, and service logic; enhance global error handling for file deletions

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -7,20 +7,23 @@ import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
 import { cloudinaryDelete } from "../config/cloudinary.config";
+import { deleteUploadedFilesFromGlobalErrorHandler } from "../utils/deleteUploadedFilesFromGlobalErrorHandler";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const globalErrorHandler = async(err: any, req: Request, res: Response, next: NextFunction) => {
     if (envVars.NODE_ENV === "development") {
         console.error("Error from globalErrorHandler:", err);
     }
 
-    if (req.file) {
-        await cloudinaryDelete(req.file.path);
-    }
+    // if (req.file) {
+    //     await cloudinaryDelete(req.file.path);
+    // }
 
-    if (req.files && Array.isArray(req.files) && req.files.length > 0) {
-        const imageUrls = req.files.map((file) => file.path);
-        await Promise.all(imageUrls.map((url) => cloudinaryDelete(url)));
-    }
+    // if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    //     const imageUrls = req.files.map((file) => file.path);
+    //     await Promise.all(imageUrls.map((url) => cloudinaryDelete(url)));
+    // }
+
+    await deleteUploadedFilesFromGlobalErrorHandler(req);
 
     let errorSources: TErrorSources[] = []
     let statusCode: number = status.INTERNAL_SERVER_ERROR;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,7 @@ import { AdminRoutes } from "../module/admin/admin.route";
 import { ScheduleRoutes } from "../module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../module/appointment/appointment.route";
+import { PatientRoutes } from "../module/patient/patient.route";
 
 const router = Router()
 
@@ -18,5 +19,6 @@ router.use("/specialties", SpecialtyRoute)
 router.use("/schedules", ScheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use("/appointments", AppointmentRoutes)
+router.use("/patients", PatientRoutes)
 
 export const IndexRoute = router;
